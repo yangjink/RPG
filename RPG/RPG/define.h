d02c66ce0965ddc654690ac1bf53b4ac92aec819
yangjink <18792461846@163.com>
2017-08-13 20:37:37 +0800

    windows

---
+++ RPG/RPG/define.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <list>
+#include <fstream>
+#include <windows.h>
+#include <math.h>
+using namespace std ;
+
+class Role;
+class Monster;
+class Mission;
+class Skill;
+class Using;
+class Weapon;
+class NPC;
+class Map;
---
+++ RPG/RPG/main.cpp
@@ -0,0 +1,6 @@
+#include "define.h"
+
+int main()
+{
+	return 0;
+}
