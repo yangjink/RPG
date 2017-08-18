6c59305fc503c47ad5e7ad6a6a6b10541da2e9e8
yangjink <18792461846@163.com>
2017-08-18 19:15:16 +0800

    人物基本属性框架的Set,Get,Change

---
+++ RPG/RPG/role.cpp
@@ -0,0 +1,293 @@
+#pragma once
+#include "role.h"
+
+//设置信息
+int Role::SetName(string n)
+{
+	_name = n;
+	return 1;
+}
+int Role::SetMoney(int m)
+{
+	_money = m;
+	return m;
+}
+int Role::SetHP(int hp)
+{
+	_HP = hp;
+	return _HP;
+}
+int Role::SetHPmax(int hpm)
+{
+	_HPmax = hpm;
+	return _HPmax;
+}
+int Role::SetMP(int mp)
+{
+	_MP = mp;
+	return _MP;
+}
+int Role::SetMPmax(int mpm)
+{
+	_MPmax = mpm;
+	return _MPmax;
+}
+int Role::SetExp(int exp)
+{
+	_exp = exp;
+	return _exp;
+}
+int Role::SetExpMax(int expm)
+{
+	_expMax = expm;
+	return _expMax;
+}
+int Role::SetLevel(int l)
+{
+	_level = l;
+	return l;
+}
+int Role::SetAttackPoint(int attack)
+{
+	_attackPoint = attack;
+	return _attackPoint;
+}
+int Role::SetDefendPoint(int defend)
+{
+	_defendPoint = defend;
+	return _defendPoint;
+}
+
+//返回基本信息
+string Role::GetName()
+{
+	return _name;
+}
+int Role::GetMoney()
+{
+	return _money;
+}
+int Role::GetHP()
+{
+	return _HP;
+}
+int Role::GetHPmax()
+{
+	return _HPmax;
+}
+int Role::GetMP()
+{
+	return _MP;
+}
+int Role::GetMPmax()
+{
+	return _MPmax;
+}
+int Role::GetExp()
+{
+	return _exp;
+}
+int Role::GetExpMax()
+{
+	return _expMax;
+}
+int Role::GetLevel()
+{
+	return _level;
+}
+int Role::GetAttackPoint()
+{
+	return _attackPoint;
+}
+int Role::GetDefendPoint()
+{
+	return _defendPoint;
+}
+
+//改变属性,再设定一个参数，1打印，0不打印
+int Role::ChangeMoney(const int& changemoney)
+{
+	_money += changemoney;
+	cout << "你获得了:" << changemoney << "银两" << endl;
+	return changemoney;
+}
+int Role::ChangeName(const string changename)
+{
+	_name = changename;
+	return 1;
+}
+int Role::ChangeHP(const int& hp,const int& tag)
+{
+	int tmp = hp;
+	if (tmp >= _HPmax - _HP)
+	{
+		tmp = _HPmax - _HP;
+	}
+	_HP += tmp;
+	if (tag && hp > 0)
+	{
+		cout << "你回复了" << tmp << "点生命" << endl;
+		Sleep(SLEEPTIME);
+	}
+
+	return _HP;
+}
+int Role::ChangeHPmax(const int& hpm)
+{
+	_HPmax += hpm;
+	return _HPmax;
+}
+//失败返回-1,因为有可能刚好使用完之后剩0MP
+int Role::ChangeMP(const int& mp, const int& tag)
+{
+	int tmp = mp;
+	if((tmp + _MP) < 0)
+	{
+		return -1;
+	}
+	if (tmp >= (_MPmax - _MP))
+	{
+		tmp = _MPmax - _MP;
+	}
+	_MP += tmp;
+	if (tag)
+	{
+		cout << "你回复了" << tmp << "点魔法值" << endl;
+		Sleep(SLEEPTIME);
+	}
+
+	return _MP;
+}
+
+int Role::ChangeExpMax(const int& exp)
+{
+	_expMax += exp;
+	return _expMax;
+}
+
+int Role::ChangeExp(const int& exp, const int& tag)
+{
+	int tmp = exp;
+	if (tag)
+	{
+		cout << "你获得了" << exp << "点经验" << endl;
+		Sleep(SLEEPTIME);
+	}
+	while (tmp > (_expMax - _exp))
+	{
+		tmp -= (_expMax - _exp);
+		LevelUp();
+	}
+	_exp += tmp;
+	return 1;
+}
+
+//打印角色基本信息
+int Role::PrintHead()
+{
+	int pointlength = 40;//头部长度
+	int temp;
+	system("cls");
+	cout << _name << "   等级: " << _level << "   银两: " << _money << endl;
+	cout << "HP: ";
+	temp = int(float(_HP)/float(_HPmax)*pointlength);
+	for (int i = 0; i < pointlength;i++)
+	{
+		if (i < temp)
+		{
+			cout << "|";
+		}
+		else
+		{
+			cout << ".";
+		}
+	}
+	cout << endl;
+	//mp
+	cout << "MP: ";
+	temp = int(float(_MP) / float(_MPmax)*pointlength);
+	for (int i = 0; i < pointlength; i++)
+	{
+		if (i < temp)
+		{
+			cout << "|";
+		}
+		else
+		{
+			cout << ".";
+		}
+	}
+	cout << endl;
+
+	cout << "EXP: "<<_exp<<"/"<<_expMax<<endl;
+	
+	for (int i = 0; i < pointlength + 10; i++)
+		cout << "-";
+	cout << endl;
+
+	return 1;
+}
+
+//升级
+int Role::LevelUp()
+{
+	_expMax = (20 * _level + 40);
+	//属性升级
+	_HPmax += 20;
+	_HP = _HPmax;
+	_MPmax += 10;
+	_MP = _MPmax;
+	_attackPoint += 3;
+	_defendPoint += 1;
+	_level++;
+	cout << _name << "升到了" << _level << "级！" << endl;
+
+	Sleep(SLEEPTIME);
+	return _level;
+}
+//判断死亡
+bool Role::Dead()
+{
+	if (_HP <= 0)
+		return true;
+	else
+		return false;
+}
+//新开始
+int Role::NewLife()
+{
+	int temp = _expMax / 10;
+	if (temp < _exp)
+	{
+		_exp -= temp;
+	}
+	else
+	{
+		temp = _exp;
+		_exp = 0;
+	}
+	_HP = _HPmax;
+	cout << "    " << _name << "已经挂掉！损失经验：" << temp << "点" << endl;
+	Sleep(SLEEPTIME);
+	
+	return 1;
+}
+
+
+int Role::Initialize()
+{
+	_name = "yjk";
+	_money = 30;
+	_HPmax = 40;
+	_HP = _HPmax;
+	_MPmax = 20;
+	_MP = _MPmax;
+	_expMax = 20;
+	_exp = 0;
+	_level = 1;
+	_attackPoint = 8;
+	_defendPoint = 2;
+	posX = 0;
+	posY = 0;
+	return 1;
+}
--- RPG/RPG/role.h
+++ RPG/RPG/role.h
@@ -1,5 +1,72 @@
 #pragma once
 #include "define.h"
-
+//人物模型
+//
 class Role
-{};
+{
+private:
+	string _name;
+	int _money;
+	int _HP;
+	int _HPmax;
+	int _MP;
+	int _MPmax;
+	int _expMax;
+	int  _exp;
+	int _level;
+	int _attackPoint;
+	int _defendPoint;
+
+	int posX;//当前位置
+	int posY;
+
+	//角色的背包
+	//身上的装备
+	//技能
+public:
+	//设置信息
+	int SetName(string);
+	int SetMoney(int);
+	int SetHP(int);
+	int SetHPmax(int);
+	int SetMP(int);
+	int SetMPmax(int);
+	int SetExp(int);
+	int SetExpMax(int);
+	int SetLevel(int);
+	int SetAttackPoint(int);
+	int SetDefendPoint(int);
+	//得到基本信息
+	string GetName();
+	int GetMoney();
+	int GetHP();
+	int GetHPmax();
+	int GetMP();
+	int GetMPmax();
+	int GetExp();
+	int GetExpMax();
+	int GetLevel();
+	int GetAttackPoint();
+	int GetDefendPoint();
+	//改变属性,再设定一个参数，1打印，0不打印
+	int ChangeName(const string);
+	int ChangeMoney(const int& );
+	int ChangeHP(const int&, const int& tag = 0);
+	int ChangeHPmax(const int& );
+	int ChangeMP(const int&, const int& tag = 0);
+	int ChangeMPmax(const int& );
+	int ChangeExp(const int&,const int& tag = 0 );//同时考虑升级的事情
+	int ChangeExpMax(const int& );
+	//打印基本角色信息
+	int PrintHead();
+	//打印更多信息
+	int PrintInfo();
+	//角色升级
+	int LevelUp();
+	//判断角色是否活着
+	bool Dead();
+	//如果死亡，开始新的历程，其实只是减少一些exp
+	int NewLife();
+	//游戏开始，初始化
+	int Initialize();
+};
