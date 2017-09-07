c81cccb42095090b7809bf407d81963dd92bc5a6
yangjink <18792461846@163.com>
2017-09-07 11:15:13 +0800

    战斗系统完结，开始任务系统

---
+++ RPG/RPG/mission.cpp
@@ -0,0 +1,129 @@
+#pragma once
+#include "misssion.h"
+
+Mission::Mission()
+{
+	_ID = 0;
+	_name = " no string ";
+	_info = " no string ";
+	_state = 0;
+	_lock = 0;
+	_circle = 1;
+	_level = 0;
+	_unlockmap = -1;
+}
+//…Ë÷√ Ù–‘
+int Mission::SetTalkBefore(string& talk)
+{
+	_talkbefore = talk;
+	return 1;
+}
+int Mission::SetTalkBeing(string& talk)
+{
+	_talkbeing = talk;
+	return 1;
+}
+int Mission::SetTalkDone(string& talk)
+{
+	_talkdone = talk;
+	return 1;
+}
+int Mission::SetMsnInfo(string& info)
+{
+	_info = info;
+	return 1;
+}
+int Mission::PushGiftList(int id)
+{
+	_giftList.push_back(id);
+	return 1;
+}
+int Mission::ClearGiftList()
+{
+	_giftList.clear();
+	return 1;
+}
+int Mission::SetLock(int lk)
+{
+	_lock = lk;
+	return _lock;
+}
+int Mission::SetUnlockMap(int id)
+{
+	_unlockmap = id;
+	return id;
+}
+int Mission::SetState(int ss)
+{
+	_state = ss;
+	return ss;
+}
+int Mission::SetLevel(int le)
+{
+	_level = le;
+	return le;
+}
+int Mission::SetID(int id)
+{
+	_ID = id;
+	return id;
+}
+int Mission::SetName(string name)
+{
+	_name = name;
+	return 1;
+}
+int Mission::SetCircle(int cir)
+{
+	_circle = cir;
+	return 1;
+}
+int Mission::GetID()
+{
+	return _ID;
+}
+string Mission::GetName()
+{
+	return _name;
+}
+int Mission::GetState()
+{
+	return _state;
+}
+
+list<int> Mission::GetGiftList()
+{
+	return _giftList;
+}
+string Mission::GetTalkBefore()	
+{
+	return _talkbefore;
+}
+string Mission::GetTalkBeing()
+{
+	return _talkbeing;
+}
+string Mission::GetTalkDone()	
+{
+	return _talkdone;
+}
+string Mission::GetInfo()
+{
+	return _info;
+}
+int Mission::GetCircle()
+{
+	return _circle;
+}
+int Mission::GetLock()
+{
+	return _lock;
+}
+int Mission::GetUnlockMap()
+{
+	return _unlockmap;
+}
+int Mission::GetLevel()
+{
+	return _level;
+}
--- RPG/RPG/misssion.h
+++ RPG/RPG/misssion.h
@@ -1,5 +1,70 @@
 #pragma once
 #include "define.h"
 
+enum msn_state
+{
+	//任务状态
+	MSN_BEFORE,
+	MSN_BEING,
+	MSN_DONE,
+	MSN_NONE
+};
+enum lock
+{
+	MSN_LOCK,
+	MSN_UNLOCK
+};
+//是否是循环任务
+enum msn_cir
+{
+	MSN_NOCIR,
+	MSN_CIRCLE
+};
 class Mission
-{};
+{
+private:
+	int _ID;
+	string _name;
+	string _info;
+	int _state;
+	int _lock;
+	int _circle;
+	int _level;
+
+	int _unlockmap;
+	list<int> _giftList;
+	list<int>::iterator _itr_gift;
+	//根据任务的状态不同，对话的内容
+	string _talkbefore;
+	string _talkbeing;
+	string _talkdone;
+public:
+	//设置属性
+	int SetTalkBefore(string&);
+	int SetTalkBeing(string&);
+	int SetTalkDone(string&);
+	int SetMsnInfo(string&);
+	int PushGiftList(int);
+	int ClearGiftList();
+	int SetLock(int);
+	int SetUnlockMap(int);
+	int SetState(int);
+	int SetLevel(int);
+	int SetID(int);
+	int SetName(string);
+	int SetCircle(int);
+	
+	int GetID();
+	string GetName();
+	int GetState();
+
+	list<int> GetGiftList();
+	string GetTalkBefore();	
+	string GetTalkBeing();	
+	string GetTalkDone();	
+	string GetInfo();	
+	int GetCircle(); 
+	int GetLock();
+	int GetLevel();
+	int GetUnlockMap();
+};
